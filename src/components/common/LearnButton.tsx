c639c9cf037877af6409f45664a5a11d37f1baa5
truongnm-0478 <ngo.mau.truong@sun-asterisk.com>
2025-05-10 11:34:33 +0700

    feat: ui component learn now button

---
+++ src/components/common/LearnButton.tsx
@@ -0,0 +1,43 @@
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { achievementApi } from "@/services/api/achievementApi";
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import { useTranslation } from "react-i18next";
+interface LearnButtonProps {
+  title: string;
+  onLearn: () => void;
+}
+
+const LearnButton: React.FC<LearnButtonProps> = ({ title, onLearn }) => {
+  const { t } = useTranslation();
+  const { data: achievements } = useQuery({
+    queryKey: ["achievements"],
+    queryFn: () => achievementApi.getAchievement(),
+  });
+
+  const streakDay = achievements?.find((achievement) => achievement.achievementType === "STREAK_DAYS");
+  const streakDayValue = streakDay?.achievementValue;
+
+  console.log(streakDayValue);
+
+  return (
+    <Card className="p-4 flex flex-col gap-4 justify-center items-center">
+      {streakDayValue && streakDayValue > 0 && (
+        <div className="flex items-center justify-center">
+          <span className="text-md font-semibold">{t("common.streak")}: &nbsp;
+            <span className="text-xl font-bold text-orange-500">{streakDayValue} </span>
+          </span>
+          <img
+            src="../app/images/icon/fire.png"
+            alt="learn"
+            className="w-8 h-8"
+          />
+        </div>
+      )}
+      <Button onClick={onLearn} className="w-full">{t("common.learn_now")}</Button>
+    </Card>
+  );
+};
+
+export default LearnButton;
---
+++ src/dataHelper/achievement.dataHelper.ts
@@ -0,0 +1,6 @@
+export interface Achievement {
+  achievementType: string;
+  achievementDate: string;
+  achievementValue: number;
+}
+
